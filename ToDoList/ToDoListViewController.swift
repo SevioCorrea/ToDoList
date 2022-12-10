6398ff0ba502c5437bd8eee148b8acedcd2fe029
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-12-10 14:10:10 -0300

    Adding TableView

---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Sévio Basilio Corrêa on 10/12/22.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = ["Item 1", "Item 2", "Item 3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - TableView Datasource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Add new Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        
+        let alert = UIAlertController(title: "Adicione novo item", message: "", preferredStyle: .alert)
+        let actionButton = UIAlertAction(title: "Adicionar item", style: .default) { action in
+            self.itemArray.append(textField.text ?? "Novo Item")
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Digite o novo item"
+            textField = alertTextField
+        }
+        
+         
+        
+        alert.addAction(actionButton)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+
+
+}
+
